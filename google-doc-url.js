43aafdfb9b447688bc0823d823237931bd9946ff
TatumT12 <144953114+TatumT12@users.noreply.github.com>
2023-09-13 15:35:55 -0400

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ZxvU8eGyuN9M8GxTU9acKVJv70iC3px_m3EVFsOHN9g/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQAU0QEtDLf_ooTGYyS5vxpmXVdhoFbI4Qx3vtv60xiY5AcA3CWjO0UvPwJRvqHMA1UxPBpo95w4_rB/pubhtml';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
